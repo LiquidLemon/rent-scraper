3e0a5f80d4ecf15664eb1e4667974848ac0c75bf
LiquidLemon <liquidlemon@protonmail.com>
2022-03-31 22:05:12 +0200

    Add support for Otodom

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
+import json
 import os
 from dataclasses import dataclass
 from datetime import datetime
 import sqlite3
-from typing import List, Dict, Callable
-from urllib.parse import urlsplit, urlunsplit
+from typing import List, Dict, Callable, Optional
+from urllib.parse import urlsplit, urlunsplit, urljoin, parse_qs, urlencode
 
 import requests
 from bs4 import BeautifulSoup
@@ -98,8 +99,48 @@ def pushbullet_send(offer: Offer):
     assert response.ok
 
 
+def get_otodom_offers(url: str) -> List[Offer]:
+    offers = set()
+
+    current_url = url
+
+    while True:
+        response = requests.get(current_url)
+        assert response.ok
+        page = BeautifulSoup(response.text, features="html.parser")
+
+        listings = page.find_all("a", {"data-cy": "listing-item-link"})
+
+        for listing in listings:
+            title = listing.find("h3").get_text()
+            url = normalize_url(urljoin("https://www.otodom.pl", listing["href"]))
+            offer = Offer(title=title, url=url)
+            offers.add(offer)
+
+        data = json.loads(page.find("script", id="__NEXT_DATA__").text)
+
+        try:
+            pagination = data["props"]["pageProps"]["data"]["searchAds"]["pagination"]
+            page = pagination["page"]
+            total_pages = pagination["totalPages"]
+
+            if page < total_pages:
+                split = urlsplit(current_url)
+                query = parse_qs(split.query)
+                query["page"] = [str(page + 1)]
+                new_query = urlencode(query, doseq=True)
+                current_url = urlunsplit((split.scheme, split.netloc, split.path, new_query, None))
+            else:
+                break
+        except TypeError:
+            break
+
+    return list(offers)
+
+
 HANDLERS: Dict[str, Callable[[str], List[Offer]]] = {
-    "www.olx.pl": get_olx_offers
+    "www.olx.pl": get_olx_offers,
+    "www.otodom.pl": get_otodom_offers,
 }
 
 
@@ -119,7 +160,7 @@ def main():
 
     queries = [
         "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/gdansk/?search%5Bdistrict_id%5D=99",
-        # url = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/gdansk/?search%5Bdist%5D=2&search%5Bdistrict_id%5D=99"
+        "https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/gdansk/wrzeszcz?distanceRadius=0&page=1&limit=36&market=ALL&locations=%5Bdistricts_6-30%5D&roomsNumber=%5BONE%2CTWO%5D&viewType=listing&lang=pl&searchingCriteria=wynajem&searchingCriteria=mieszkanie&searchingCriteria=cala-polska",
     ]
 
     offers = set()
